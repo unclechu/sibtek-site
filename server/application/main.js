9d93bcd3b11a7b49b37c1060722ab6b1a98318d4
AndrwFn <fn.andrw@gmail.com>
2015-02-03 22:11:53 +0600

    Removed build from git index, change .gitignore

--- server/application/main.js
+++
@@ -1,8 +0,0 @@
-var express, jade, http;
-express = require('express');
-jade = require('jade');
-http = require('http');
-module.exports = {
-  site: express(),
-  admin: express()
-};
--- server/application/site/module.js
+++
@@ -1,4 +0,0 @@
-var express, site;
-express = require('express');
-site = express();
-module.exports = site;
